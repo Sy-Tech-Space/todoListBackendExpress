379544331c2332ed3f263e2e3c9c810ae760170d
RozaHasso <rozaheso@gmail.com>
2024-04-14 23:04:42 +0200

    Push  test

--- todoListBackend/src/main/java/sy/tech/space/todoListBackend/config/dbConfig.js
+++ todoListBackend/src/main/java/sy/tech/space/todoListBackend/config/dbConfig.js
@@ -1,34 +1,36 @@
 // config/dbConfig.js
-// const sql = require('postgresql');
+/*const sql = require('mssql');
 
-// const config = {
-//     server: 'localhost',  // Change this to your server address
-//     port: 5433,           // Change this to your port number if necessary
-//     database: 'address', // Change this to your database name
-//     username: 'postgres',
-//     password: 'postgres',
-//     options: {
-//         encrypt: false,   // Set to true if using Azure SQL
-//         trustServerCertificate: true, // For self-signed certificates, if applicable
-//         // Set integratedSecurity to true for Windows authentication
-//         integratedSecurity: true
-//     }
-// };
+const config = {
+    server: 'localhost',  // Change this to your server address
+    port: 1433,           // Change this to your port number if necessary
+    database: 'todoList', // Change this to your database name
+    options: {
+        encrypt: false,   // Set to true if using Azure SQL
+        trustServerCertificate: true, // For self-signed certificates, if applicable
+        // Set integratedSecurity to true for Windows authentication
+        integratedSecurity: true  
+    }
+};
 
-// const poolPromise = new sql.ConnectionPool(config)
-//     .connect()
-//     .then(pool => {
-//         console.log('Connected to MSSQL Server');
-//         return pool;
-//     })
-//     .catch(err => console.error('MSSQL Server connection error:', err));
+const poolPromise = new sql.ConnectionPool(config)
+    .connect()
+    .then(pool => {
+        console.log('Connected to MSSQL Server');
+        return pool;
+    })
+    .catch(err => console.error('MSSQL Server connection error:', err));
 
 
 
-// module.exports = {
-//     sql,
-//     poolPromise
-// };
+module.exports = {
+    sql,
+    poolPromise
+};
+*/
+
+//........................................................
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
